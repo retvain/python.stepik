d38d0ab4e084e7af242c12a413195ba67db33516
retvain <retvain@gmail.com>
2018-07-17 17:08:06 +0500

    1.12_20170717

--- 1.12.py
+++ 1.12.py
@@ -42,17 +42,92 @@
 #else:
 #    print ('Деление на 0!')
 ########################################################
-name = input('Введите фигуру \n')
-if name == 'треугольник':
-    a = float(input())
-    b = float(input())
-    c = float(input())
-    p = (a+b+c)/2
-    print((p*(p-a)*(p-b)*(p-c))**0.5)
-elif name == 'прямоугольник':
-    a = float(input())
-    b = float(input())
-    print(a*b)
-elif name == 'круг':
-    r = float(input())
-    print(3.14*(r**2))
+#name = input('Введите фигуру \n')
+#if name == 'треугольник':
+#    a = float(input())
+#    b = float(input())
+#    c = float(input())
+#    p = (a+b+c)/2
+#    print((p*(p-a)*(p-b)*(p-c))**0.5)
+#elif name == 'прямоугольник':
+#    a = float(input())
+#    b = float(input())
+#    print(a*b)
+#elif name == 'круг':
+#    r = float(input())
+#    print(3.14*(r**2))
+########################################################
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#if a>=b and a>=c:
+#    print(a)
+#elif b>=a and b>=c:
+#    print (b)
+#else: print (c)
+#if a<=b and a<=c:
+#    print(a)
+#elif b<=a and b<=c:
+#    print (b)
+#else: print (c)
+#if (a>=b or a>=c) and (a<=b or a<=c):
+#    print (a)
+#elif (b>=a or b>=c) and (b<=a or b<=c):
+#    print (b)
+#else: print(c)
+########################################################
+#n = int(input())
+#if (n % 10 == 1 or n % 100 == 1) and (n != 11) and (n % 100 != 11):
+#    print(n, 'программист')
+#elif (2 <= (n % 10) <= 4) and not (11 <= n % 100 <= 14):
+#    print(n, 'программиста')
+#else:
+#    print(n, 'программистов')
+########################################################
+a = int(input())
+if ((a//100000) + (a%10000) + (a%1000)) == ((a%10) + (a%100) + (a%1)): print ('Счастливый')
+else: print('Обычный')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
