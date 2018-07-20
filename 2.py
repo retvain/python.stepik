63832577f62d0685396eb58691a6a07dcda175f9
retvain <retvain@gmail.com>
2018-07-20 17:07:53 +0500

    2.4

---
+++ 2.py
@@ -0,0 +1,50 @@
+##########################################
+#a = int(input())
+#b = 0
+#while a != 0:
+#    b += a
+#    a = int(input())
+#print (b)
+##########################################
+#a = int(input())
+#b = int(input())
+#n = 1
+#if a == b:
+#    print (a)
+#else:
+#    while ((n % a) != 0) or ((n % b) != 0):
+#        n += 1
+#    print(n)
+##########################################
+#n = 0
+#while n == 0:
+#    a = int(input())
+#    if a < 10:
+#        continue
+#    elif a > 100:
+#        break
+#    else: print (a)
+##########################################
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#d = int(input())
+#for e in range(c, (d+1)):
+#    print ('\t', e, end='')
+#print()
+#for i in range(a, (b+1)):
+#    print (i, end='')
+#    for j in range(c, (d+1)):
+#        print('\t', i*j, end='')
+#    print()
+##########################################
+#a = int(input())
+#b = int(input())
+#s = 0
+#n = 0
+#for i in range(a, b+1):
+#    if i%3 == 0:
+#        s += i
+#        n += 1
+#print (s/n)
+##########################################
