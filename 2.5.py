b4195b3e7d0756b49c86b656a46d3eadd5b08d77
retvain <retvain@gmail.com>
2018-07-25 22:51:33 +0500

    2.5 end

--- 2.5.py
+++ 2.5.py
@@ -6,4 +6,33 @@
 #s += n[-1]
 #print (s )
 ##############################################
+#n = [int(i) for i in input().split()]
+#t = ''
+#c = len(n) - 1
+#if c == 0:
+#    temp = str(n[0])
+#else:
+#    for i in range (c):
+#        temp = str(n[i-1] + n[i+1])
+#        t += temp + ' '
+#    temp = str(n[c-1]+n[0])
+#t += temp
+#print (t)
+##############################################
 n = [int(i) for i in input().split()]
+srt = sorted(n)
+c = len(srt) - 1
+s = ''
+temp = ''
+if c == 0:
+    s = ''
+else:
+    for i in range (c):
+        if srt[i] == srt [i+1]:
+            temp = str(srt[i]) + ' '
+        else:
+            s += temp
+            temp = ''
+    if srt[-1] == srt[-2]:
+        s += str(srt[-1])
+print (s)
